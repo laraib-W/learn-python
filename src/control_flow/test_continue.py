b030d3cefa0e71fed4b1e0c42006ef05d60b71d2
LaraibWaheed <laraibwaheed@A006-00943.local>
2022-08-05 20:48:11 +0500

    Control Flow Correcions

--- src/control_flow/test_continue.py
+++ src/control_flow/test_continue.py
@@ -8,16 +8,13 @@ The continue statement is borrowed from C, continues with the next iteration of 
 
 def test_continue_statement():
     """CONTINUE statement in FOR loop"""
-
-    # Let's
-
     # This list will contain only even numbers from the range.
     even_numbers = []
     # This list will contain every other numbers (in this case - ods).
     rest_of_the_numbers = []
 
     for number in range(0, 10):
-        # Check if remainder after division is zero (which would mean that number is even).
+        # Check if remainder after division is not zero (which would mean that number is odd).
         if number % 2:
             rest_of_the_numbers.append(number)
             # Stop current loop iteration and go to the next one immediately.
